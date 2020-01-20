96ed4b89ef674d1b09cc1c46b0cd0528565a2733
huma <yuto101007@gmail.com>
2020-01-20 10:28:41 +0900

    vuex整備

--- src/components/Ball.vue
+++ src/components/Ball.vue
@@ -22,6 +22,7 @@
 <script> 
 
 import GD from '@/assets/GrobalData' 
+import { mapState } from 'vuex'
 
 export default { 
   name: 'Ball', 
@@ -46,7 +47,8 @@ export default {
       corn:{x:0,y:0} 
     } 
   }, 
-  computed: { 
+  computed: {
+    ...mapState('manager',['fps']),
     posStyle: function () { 
       return {top: 'calc(' + this.pos.y + '% - ' + this.size / 2 + 'px)', left: 'calc(' + this.pos.x + '% - ' + this.size / 2 + 'px)'} 
     }, 
@@ -59,7 +61,8 @@ export default {
       return {top: 'calc(' + y + '% - ' + 2 + 'px)', left: 'calc(' + x + '% - ' + 2 + 'px)'} 
     }, 
 
-    run: function () { 
+    run: function () {
+      console.log(this.fps)
       //力をかける 
       var force = {x:0, y:0} 
       if (this.pressedKey["DownArrow"]){ 
--- src/store/manager.js
+++ src/store/manager.js
@@ -1,14 +1,31 @@
 
 const state = {
-
+    fps: 120,
+    intervalID: undefined,
+    updateFlg: true,
+    width: undefined,
+    height: undefined
 }
 
 const actions = {
-
+    startUpdateAsync({ commit,state }) {
+        var id = setInterval(() => {
+            commit('inversionUpdateFlg')
+        }, 1000 / state.fps)
+        commit('setIntervalID', id)
+    },
+    stopUpdateAsync({ commit,state }) {
+        clearInterval(state.intervalID)
+    }
 }
 
 const mutations = {
-
+    inversionUpdateFlg({ state }) {
+        state.updateFlg = !state.updateFlg
+    },
+    setIntervalID({ state }, id) {
+        state.intervalID = id
+    }
 }
 
 export default {
